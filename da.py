ccc7eb8ef64066e2513edaa15f8ad5a8d48fc778
GondorGitaros <csaszardaniel81@gmail.com>
2023-12-22 23:49:08 +0100

    Changed HEIL to GG

--- da.py
+++ da.py
@@ -106,7 +106,7 @@ TT_RPAREN   	= 'RPAREN'
 TT_EOF			= 'EOF'
 
 KEYWORDS = [
-	'HEIL'
+	'GG'
 ]
 
 class Token:
@@ -378,7 +378,7 @@ class Parser:
 	def expr(self):
 		res = ParseResult()
 
-		if self.current_tok.matches(TT_KEYWORD, 'HEIL'):
+		if self.current_tok.matches(TT_KEYWORD, 'GG'):
 			res.register_advancement()
 			self.advance()
 
